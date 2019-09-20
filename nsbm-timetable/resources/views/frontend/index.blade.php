62e5853adbf9bc085b30d33fd5b6f43bfaafd536
Geeth Kavinda <37553886+Geeth1002@users.noreply.github.com>
2019-09-21 00:23:45 +0530

    change body background

--- nsbm-timetable/resources/views/frontend/index.blade.php
+++ nsbm-timetable/resources/views/frontend/index.blade.php
@@ -3,19 +3,19 @@
 @section('title', app_name() . ' | ' . __('navs.general.home'))
 
 @section('content')
-
-<div class="row mb-4">
+<body background="/img/nsbmback.jpg">
+<div class="row mb-4" style="">
 
     <div class="col" >
             <br><br>
             <div class="text-center">
                 <h2>Welcome to</h2>
                 <h1><b>NSBM Green University Town</b></h1><br><br><br>
-            <img src="{{ URL:: to ('/') }}/img/nsbm.jpg" style="height:200px; width:450px;"/>
-                <br><br><br><br>
+                    <img src="{{ URL:: to ('/') }}/img/nsbm.png" style="height:200px; width:450px;"/>
+                <br><br><br><br><br>
                 <h3>Smart Time Table Management System</h3><br>
             </div>
     </div><!--col-->
 </div><!--row-->
-
+</body>
 @endsection
