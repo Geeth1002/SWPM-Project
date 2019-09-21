51d21ab0edf672f5e5d52a00c374e260d485b01d
Geeth Kavinda <37553886+Geeth1002@users.noreply.github.com>
2019-09-21 14:31:13 +0530

    insert data into table

--- nsbm-timetable/app/Http/Controllers/TimetableController.php
+++ nsbm-timetable/app/Http/Controllers/TimetableController.php
@@ -9,6 +9,14 @@ class TimetableController extends Controller
 {
     public function store(Request $request){
        //dd($request->all());
-       
+       $timetable=new Timetable;
+       $timetable->batch=$request->mbatch;
+       $timetable->hall=$request->mhall;
+       $timetable->module=$request->mmodule;
+       $timetable->lecturer=$request->mlecture;
+       $timetable->date=$request->mdate;
+       $timetable->time=$request->mtime;
+       $timetable->save();
+       return redirect()->back();
     }
 }
