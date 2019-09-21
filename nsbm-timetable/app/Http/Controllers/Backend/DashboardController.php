fa7afa3b9980aa44afb249d74a9654e7200356fc
Geeth Kavinda <37553886+Geeth1002@users.noreply.github.com>
2019-09-21 23:52:03 +0530

    update admin panel table view

--- nsbm-timetable/app/Http/Controllers/Backend/DashboardController.php
+++ nsbm-timetable/app/Http/Controllers/Backend/DashboardController.php
@@ -1,6 +1,7 @@
 <?php
 use Illuminate\Support\Facades\DB;
 namespace App\Http\Controllers\Backend;
+use Illuminate\Support\Facades\DB;
 
 use App\Http\Controllers\Controller;
 use App\Timetable;
@@ -15,9 +16,9 @@ class DashboardController extends Controller
      */
     public function index()
     {
-       $data = Timetable::all();
+       //$data = Timetable::all();
 
-
+       $data = DB::table('timetables')->whereDate('date', '=', date('Y-m-d'))->get();
 
        return view('backend.dashboard')->withTimeTable($data);
 
