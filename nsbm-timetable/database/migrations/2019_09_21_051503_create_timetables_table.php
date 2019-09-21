1ad29334f3e193f2c961c8ae46345caee1db9af8
Thameera Malinga <33679537+Thameera1002@users.noreply.github.com>
2019-09-21 11:04:01 +0530

    model timetable & migration created by TM

---
+++ nsbm-timetable/database/migrations/2019_09_21_051503_create_timetables_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTimetablesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('timetables', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('batch');
+            $table->string('hall');
+            $table->string('module');
+            $table->string('lecturer');
+            $table->string('date');
+            $table->string('time');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('timetables');
+    }
+}
