e9ced39268386cfc2b2c5b9785fef46562e1432f
Geeth Kavinda <37553886+Geeth1002@users.noreply.github.com>
2019-09-21 16:19:04 +0530

    dd retruve data from table

--- nsbm-timetable/resources/views/backend/dashboard.blade.php
+++ nsbm-timetable/resources/views/backend/dashboard.blade.php
@@ -33,4 +33,40 @@
                         <input type="submit" class="btn btn-primary" value="Submit">
                         <input type="button" class="btn btn-warning" value="Clear">
                     </form>
+
+                    <diV class="container">
+
+                            <div class="text-center">
+
+                                <div class="row">
+
+                                    <div class="col-md-12">
+
+                                            <br><br><br>
+                                            <table>
+                                                    <th><h1>Time Table &emsp13;-&emsp13;</h1></th>
+                                                    <th><h1>2019-09-11</h1></th>
+                                            </table>
+                                                    <br>
+
+                                        <table class="table table-light">
+                                            <th>Batch Name</th>
+                                            <th>Hall/Lab</th>
+                                            <th>Module</th>
+                                            <th>Lecture</th>
+                                            <th>Time</th>
+
+                                            <tr>
+                                                <td>17.1 UGC</td>
+                                                <td>L-102</td>
+                                                <td>SWPM</td>
+                                                <td>Mr. Chamen</td>
+                                                <td>9.00</td>
+                                            </tr>
+
+                                        </table>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
 @endsection
