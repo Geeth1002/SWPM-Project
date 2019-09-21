b615844ba8783348bf5f6e74f6e82843ba9a740d
Geeth Kavinda <37553886+Geeth1002@users.noreply.github.com>
2019-09-21 13:07:21 +0530

    admin  dashboard clean

--- nsbm-timetable/resources/views/backend/includes/footer.blade.php
+++ nsbm-timetable/resources/views/backend/includes/footer.blade.php
@@ -1,11 +1,11 @@
 <footer class="app-footer">
     <div>
         <strong>@lang('labels.general.copyright') &copy; {{ date('Y') }}
-            <a href="http://laravel-boilerplate.com">
-                @lang('strings.backend.general.boilerplate_link')
+            <a href="http://www.nsbm.lk">
+               NSBM Green University
             </a>
         </strong> @lang('strings.backend.general.all_rights_reserved')
     </div>
 
-    <div class="ml-auto">Theme by <a href="http://coreui.io">CoreUI</a></div>
+    <div class="ml-auto">Design By Team Annonymous</div>
 </footer>
