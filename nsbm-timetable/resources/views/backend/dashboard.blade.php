0e1191fe92ac1cb8d0c0ce519a1fad217866cbc1
Geeth Kavinda <37553886+Geeth1002@users.noreply.github.com>
2019-09-21 13:40:42 +0530

    admin dashboard form

--- nsbm-timetable/resources/views/backend/dashboard.blade.php
+++ nsbm-timetable/resources/views/backend/dashboard.blade.php
@@ -3,5 +3,29 @@
 @section('title', app_name() . ' | ' . __('strings.backend.dashboard.title'))
 
 @section('content')
-    
+<diV class="container">
+
+        <div class="text-center">
+
+            <br><h2>Welcome Admin</h2><br>
+            <br><h4>Create / Update Timetables</h4><br><br>
+
+
+            <div class="row">
+
+                <div class="col-md-12">
+                    <form method="post" action="/saveTask">
+                    {{csrf_field()}}
+
+
+                        <input type="text" class="form-control" name="mbatch" placeholder="Batch Name"><br>
+                        <input type="text" class="form-control" name="mhall" placeholder="Hall/Lab"><br>
+                        <input type="text" class="form-control" name="mmodule" placeholder="Module"><br>
+                        <input type="text" class="form-control" name="mlecture" placeholder="Lecture"><br>
+                        <input type="text" class="form-control" name="mdate" placeholder="Date"><br>
+                        <input type="text" class="form-control" name="mtime" placeholder="Time"><br>
+                        <br>
+                        <input type="submit" class="btn btn-primary" value="Submit">
+                        <input type="button" class="btn btn-warning" value="Clear">
+                    </form>
 @endsection
