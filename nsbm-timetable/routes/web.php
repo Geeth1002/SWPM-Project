0fcefc120678fcbb47b80ce4b9832d3f703d950b
Thameera Malinga <33679537+Thameera1002@users.noreply.github.com>
2019-09-29 01:33:42 +0530

    Routes files cleared (Remove unwanted comments)

--- nsbm-timetable/routes/web.php
+++ nsbm-timetable/routes/web.php
@@ -5,46 +5,23 @@ use App\Http\Controllers\LanguageController;
 use App\Http\Controllers\TimetableController;
 use App\Timetable;
 
-/*
- * Global Routes
- * Routes that are used between both frontend and backend.
- */
-
 // Switch between the included languages
 Route::get('lang/{lang}', [LanguageController::class, 'swap']);
 
-/*
- * Frontend Routes
- * Namespaces indicate folder structure
- */
 Route::group(['namespace' => 'Frontend', 'as' => 'frontend.'], function () {
     include_route_files(__DIR__.'/frontend/');
 });
 
-/*
- * Backend Routes
- * Namespaces indicate folder structure
- */
 Route::group(['namespace' => 'Backend', 'prefix' => 'admin', 'as' => 'admin.', 'middleware' => 'admin'], function () {
-    /*
-     * These routes need view-backend permission
-     * (good if you want to allow more than one group in the backend,
-     * then limit the backend features by different roles or permissions)
-     *
-     * Note: Administrator has all permissions so you do not have to specify the administrator role everywhere.
-     * These routes can not be hit if the password is expired
-     */
     include_route_files(__DIR__.'/backend/');
 });
 Route::get('/timetableview', function () {
-    //$date=date('Y-m-d');
-    //return view('\frontend\user\timetableview');
-   //$data = Timetable::all();
+ 
     if(Auth::check()){
         $data = DB::table('timetables')->whereDate('date', '=', date('Y-m-d'))->get();
         return view('frontend.user.timetableview')->withTimeTable($data);
     }
-else{
+    else{
 
         return redirect('/login')->with('msg','Logged in needed');
     }
