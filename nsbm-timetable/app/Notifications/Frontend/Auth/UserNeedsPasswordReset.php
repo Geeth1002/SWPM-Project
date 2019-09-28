c0c97014ae33f6d3a0710f03990eeaf1d084ffcb
Thameera Malinga <33679537+Thameera1002@users.noreply.github.com>
2019-09-29 01:27:15 +0530

    Notification files cleared (Remove unwanted comments)

--- nsbm-timetable/app/Notifications/Frontend/Auth/UserNeedsPasswordReset.php
+++ nsbm-timetable/app/Notifications/Frontend/Auth/UserNeedsPasswordReset.php
@@ -6,49 +6,22 @@ use Illuminate\Bus\Queueable;
 use Illuminate\Notifications\Notification;
 use Illuminate\Notifications\Messages\MailMessage;
 
-/**
- * Class UserNeedsPasswordReset.
- */
 class UserNeedsPasswordReset extends Notification
 {
     use Queueable;
 
-    /**
-     * The password reset token.
-     *
-     * @var string
-     */
     public $token;
 
-    /**
-     * UserNeedsPasswordReset constructor.
-     *
-     * @param $token
-     */
     public function __construct($token)
     {
         $this->token = $token;
     }
 
-    /**
-     * Get the notification's channels.
-     *
-     * @param mixed $notifiable
-     *
-     * @return array|string
-     */
     public function via($notifiable)
     {
         return ['mail'];
     }
 
-    /**
-     * Build the mail representation of the notification.
-     *
-     * @param mixed $notifiable
-     *
-     * @return \Illuminate\Notifications\Messages\MailMessage
-     */
     public function toMail($notifiable)
     {
         return (new MailMessage())
